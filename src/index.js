19214ac4efc1b693d37dbca5805167ed42db7c4a
seroga213 <serogaseroga05@gmail.com>
2022-10-20 15:32:06 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Phonebook } from 'components/App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
@@ -8,7 +8,7 @@ import { theme } from './theme';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-        <App />
+        <Phonebook />
      </ThemeProvider>
   </React.StrictMode>
 );
